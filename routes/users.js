2ff0e1ff70db972ba4f04c04bab65c3daf593a61
Qseboy <Qseboy1998@gmail.com>
2022-05-31 20:09:19 +0300

    add users routes

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,6 @@
 const {Router} = require("express")
 const router = Router()
-
+const Users = require("../models/users")
 
 router.get("/", (req, res) => {
     res.render("singup", {
@@ -9,5 +9,29 @@ router.get("/", (req, res) => {
     })
 })
 
+router.post("/", async (req, res) => {
+    const users = new Users({
+        name: req.body.name,
+        lastName: req.body.lastName,
+        email: req.body.email,
+        phone: req.body.phone,
+        password: req.body.password 
+    })
+
+    //Проверка на мыло
+    const userEmail = req.body.email
+    const checkUserEmail = await Users.findOne({email: userEmail})
+
+    if(checkUserEmail) {
+        console.log("Такой имейл существует, 409")
+    } else {
+        try {
+            await users.save()
+            res.redirect("/")
+        } catch(e) {
+            console.log(e)
+        }
+    }
+})
 
 module.exports = router
